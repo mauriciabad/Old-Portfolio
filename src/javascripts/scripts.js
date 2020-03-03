c1db3c549515facb675d2b381cc0e08f2c6dcc65
Maurici <hello@mauriciabad.com>
2020-03-03 16:58:57 +0100

    Fix js compilation 2

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -6,21 +6,20 @@ import particles from '../data/particles.json';
 
 particlesJS('projects-bg', particles);
 
-let ageSpan = document.getElementById('age');
+const ageSpan = document.getElementById('age');
 let age = (Date.now() - new Date(1998, 0, 29, 0, 15)) / 31557600000;
-let lastTime;
+let lastTime, dif;
 
 function updateAge(currentTime) {
-  if(!lastTime) lastTime = currentTime;
+  if (!lastTime) lastTime = currentTime;
   dif = currentTime - lastTime;
   lastTime = currentTime;
   age += dif / 31557600000;
 
-  ageSpan.children[0].textContent = age.toFixed(10).slice(0,3);
+  ageSpan.children[0].textContent = age.toFixed(10).slice(0, 3);
   ageSpan.children[1].textContent = age.toFixed(10).slice(3);
 
   window.requestAnimationFrame(updateAge);
 }
 
 window.requestAnimationFrame(updateAge);
-
