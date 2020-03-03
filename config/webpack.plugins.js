7b12732a9893cffc927b6bb5ba0e8fd41419d873
Maurici <hello@mauriciabad.com>
2020-03-03 22:54:48 +0100

    fix copy

--- config/webpack.plugins.js
+++ config/webpack.plugins.js
@@ -105,9 +105,7 @@ const webpackBar = new WebpackBar({
   color: '#ff6469',
 });
 
-const copy = new CopyWebpackPlugin([
-  { from: 'public', to: 'dist' },
-]);
+const copy = new CopyWebpackPlugin(['/public']);
 
 // Google analytics
 const CODE = `<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create','{{ID}}','auto');ga('send','pageview');</script>`;
