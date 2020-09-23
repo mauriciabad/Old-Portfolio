1a61c1b88a8e56a3b0eb77e7e65c054ad0096e5c
Maurici <hello@mauriciabad.com>
2020-09-23 20:47:38 +0200

    Create PostPreview component

---
+++ components/PostPreview.vue
@@ -0,0 +1,91 @@
+<template>
+  <NavLink
+    class="entry-link"
+    :aria-label="'post link ' + page.title"
+    :link="page.path"
+  >
+    <article
+      itemprop="blogPost"
+      class="entry"
+      itemscope
+      itemtype="https://schema.org/BlogPosting"
+    >
+      <meta itemprop="mainEntityOfPage" :content="page.path" />
+      <img class="entry-image" :src="page.frontmatter.featuredimg" />
+      <header class="entry-header">
+        <h2 itemprop="name headline">{{ page.title }}</h2>
+      </header>
+      <section class="entry-content">
+        <p itemprop="description">
+          {{ page.frontmatter.summary || page.summary }}
+        </p>
+      </section>
+      <footer class="entry-footer">
+        <!-- <div
+              v-if="page.frontmatter.tags"
+              class="entry-tag"
+              itemprop="keywords"
+            >
+              <span>{{ tag }}</span>
+            </div> -->
+        <time
+          v-if="page.frontmatter.date"
+          pubdate
+          itemprop="datePublished"
+          :datetime="page.frontmatter.date"
+        >
+          {{ formatDate(page.frontmatter.date) }}
+        </time>
+        <!-- · ?? min -->· {{ page.frontmatter.author }}
+      </footer>
+    </article>
+  </NavLink>
+</template>
+
+<script>
+export default {
+  name: 'PostPreview',
+  props: {
+    page: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    formatDate(date) {
+      return new Intl.DateTimeFormat('en-GB', {
+        dateStyle: 'full',
+      }).format(new Date(date))
+    },
+  },
+}
+</script>
+
+<style lang="stylus">
+.entry-image
+  grid-area img
+  object-fit cover
+  border-radius 1rem 0 0 1rem
+  margin -1.5rem
+  height: calc(3rem + 100%);grid-areaimgobject-fitcoverborder-radius1rem001remmargin-1.5remheightgrid-areaimgobject-fitcoverborder-radius1rem001remmargin-1.5remheightgrid-areaimgobject-fitcoverborder-radius1rem001remmargin-1.5remheightgrid-areaimgobject-fitcoverborder-radius1rem001remmargin-1.5remheightgrid-areaimgobject-fitcoverborder-radius1rem001remmargin-1.5remheightgrid-areaimgobject-fitcoverborder-radius1rem001remmargin-1.5remheightgrid-areaimgobject-fitcoverborder-radius1rem001remmargin-1.5remheight
+
+.entry-link
+  color currentColor
+  text-decoration none
+  border-radius 1rem
+  display block
+
+.entry
+  margin 2rem
+  padding 1.5rem
+  box-shadow 0 2px 8px rgba(0,0,0,0.15)
+  border-radius 1rem
+  display grid
+  grid-template auto auto auto / 10rem auto
+  grid-template-areas "img title"\
+                      "img desc"\
+                      "img footer"
+
+.entry-header h2
+  margin 0
+</style>
--- global-components/BaseListLayout.vue
+++ global-components/BaseListLayout.vue
@@ -1,49 +1,6 @@
 <template>
   <div itemscope itemtype="http://schema.org/Blog">
-    <template v-for="page in pages">
-      <NavLink
-        :key="page.key"
-        class="entry-link"
-        :aria-label="'post link ' + page.title"
-        :link="page.path"
-      >
-        <article
-          itemprop="blogPost"
-          class="entry"
-          itemscope
-          itemtype="https://schema.org/BlogPosting"
-        >
-          <meta itemprop="mainEntityOfPage" :content="page.path" />
-          <img class="entry-image" :src="page.frontmatter.featuredimg" />
-          <header class="entry-header">
-            <h2 itemprop="name headline">{{ page.title }}</h2>
-          </header>
-          <section class="entry-content">
-            <p itemprop="description">
-              {{ page.frontmatter.summary || page.summary }}
-            </p>
-          </section>
-          <footer class="entry-footer">
-            <!-- <div
-              v-if="page.frontmatter.tags"
-              class="entry-tag"
-              itemprop="keywords"
-            >
-              <span>{{ tag }}</span>
-            </div> -->
-            <time
-              v-if="page.frontmatter.date"
-              pubdate
-              itemprop="datePublished"
-              :datetime="page.frontmatter.date"
-            >
-              {{ formatDate(page.frontmatter.date) }}
-            </time>
-            · ?? min · {{ page.frontmatter.author }}
-          </footer>
-        </article>
-      </NavLink>
-    </template>
+    <PostPreview v-for="page in pages" :key="page.key" :page="page" />
 
     <component
       :is="paginationComponent"
@@ -60,8 +17,13 @@ import {
   Pagination,
   SimplePagination,
 } from '@vuepress/plugin-blog/lib/client/components'
+import PostPreview from '@theme/components/PostPreview.vue'
 
 export default {
+  components: {
+    PostPreview,
+  },
+
   data() {
     return {
       paginationComponent: null,
@@ -91,41 +53,6 @@ export default {
 
       return Vue.component(n) || Pagination
     },
-
-    formatDate(date) {
-      return new Intl.DateTimeFormat('en-GB', {
-        dateStyle: 'full',
-      }).format(new Date(date))
-    },
   },
 }
 </script>
-
-<style lang="stylus">
-.entry-image
-  grid-area img
-  object-fit cover
-  border-radius 1rem 0 0 1rem
-  margin -1.5rem
-  height: calc(3rem + 100%);
-
-.entry-link
-  color currentColor
-  text-decoration none
-  border-radius 1rem
-  display block
-
-.entry
-  margin 2rem
-  padding 1.5rem
-  box-shadow 0 2px 8px rgba(0,0,0,0.15)
-  border-radius 1rem
-  display grid
-  grid-template auto auto auto / 10rem auto
-  grid-template-areas "img title"\
-                      "img desc"\
-                      "img footer"
-
-.entry-header h2
-  margin 0
-</style>
