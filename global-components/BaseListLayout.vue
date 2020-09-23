1f1416fd53f167cf6a828744e8106774ba48d203
mauriciabad <hello@mauriciabad.com>
2020-09-23 14:18:15 +0200

    Style blog entries

--- global-components/BaseListLayout.vue
+++ global-components/BaseListLayout.vue
@@ -1,84 +1,49 @@
 <template>
-  <div id="">
-    <div
-      class="card-panel-content row"
-      itemscope
-      itemtype="http://schema.org/Blog"
-    >
-      <article
-        v-for="page in pages"
+  <div itemscope itemtype="http://schema.org/Blog">
+    <template v-for="page in pages">
+      <NavLink
         :key="page.key"
-        itemprop="blogPost"
-        class="ui-post entry col-md-4 card-container"
-        itemscope
-        itemtype="https://schema.org/BlogPosting"
+        class="entry-link"
+        :aria-label="'post link ' + page.title"
+        :link="page.path"
       >
-        <meta itemprop="mainEntityOfPage" :content="page.path" />
-        <div class="entry card h-100">
-          <div class="card-header-image">
-            <NavLink :link="page.path">
-              <img :src="page.frontmatter.featuredimg" />
-            </NavLink>
-          </div>
-
-          <div class="card-inside">
-            <div class="card-subheading">
-              <div
-                v-if="page.frontmatter.tags"
-                class="ui-post-meta ui-post-tag"
-                itemprop="keywords"
-              >
-                <span>{{ tag }}</span>
-              </div>
-            </div>
-            <h2 class="heading" itemprop="name headline">
-              <NavLink :link="page.path" class="heading">{{
-                page.title
-              }}</NavLink>
-            </h2>
+        <article
+          itemprop="blogPost"
+          class="entry"
+          itemscope
+          itemtype="https://schema.org/BlogPosting"
+        >
+          <meta itemprop="mainEntityOfPage" :content="page.path" />
+          <img class="entry-image" :src="page.frontmatter.featuredimg" />
+          <header class="entry-header">
+            <h2 itemprop="name headline">{{ page.title }}</h2>
+          </header>
+          <section class="entry-content">
             <p itemprop="description">
               {{ page.frontmatter.summary || page.summary }}
             </p>
-          </div>
-          <div class="meta-bottom mt-auto">
-            <div v-if="$themeConfig.authors">
-              <span
-                v-for="author in $themeConfig.authors"
-                :key="author.name"
-                class="nav-item"
-              >
-                <div class="d-flex">
-                  <a class="profile-avatar">
-                    <img
-                      v-if="author.name === page.frontmatter.author"
-                      :src="$withBase(author.avatar)"
-                      class="avatar-image"
-                      :alt="author.name"
-                    />
-                  </a>
-                  <div class="meta">
-                    <div v-if="author.name === page.frontmatter.author">
-                      <span class="username">{{ author.name }}</span> &nbsp;
-                    </div>
-                    <div></div>
-                  </div>
-                </div>
-              </span>
-            </div>
-
-            <div v-if="page.frontmatter.date" class="date">
-              <time
-                pubdate
-                itemprop="datePublished"
-                :datetime="page.frontmatter.date"
-              >
-                {{ resolvePostDate(page.frontmatter.date) }}
-              </time>
-            </div>
-          </div>
-        </div>
-      </article>
-    </div>
+          </section>
+          <footer class="entry-footer">
+            <!-- <div
+              v-if="page.frontmatter.tags"
+              class="entry-tag"
+              itemprop="keywords"
+            >
+              <span>{{ tag }}</span>
+            </div> -->
+            <time
+              v-if="page.frontmatter.date"
+              pubdate
+              itemprop="datePublished"
+              :datetime="page.frontmatter.date"
+            >
+              {{ formatDate(page.frontmatter.date) }}
+            </time>
+            · ?? min · {{ page.frontmatter.author }}
+          </footer>
+        </article>
+      </NavLink>
+    </template>
 
     <component
       :is="paginationComponent"
@@ -127,38 +92,40 @@ export default {
       return Vue.component(n) || Pagination
     },
 
-    resolvePostDate(date) {
+    formatDate(date) {
       return new Intl.DateTimeFormat('en-GB', {
         dateStyle: 'full',
       }).format(new Date(date))
-    },
-
-    resolvePostTags(tags) {
-      if (!tags || Array.isArray(tags)) return tags
-      return [tags]
     },
   },
 }
 </script>
 
 <style lang="stylus">
-.pagination > li > a, .pagination > li > span
-  border:0;
-  border-bottom:1px solid #ddd
-  font-size:15px
-  font-weight:700
-.pagination > li:last-child > a, .pagination > li:last-child > span, .pagination > li:first-child > a, .pagination > li:first-child > span
-  border-radius:0
-.pagination > li > a, .pagination > li > span
-  color:inherit
-  background:none
-.pagination > .active > a, .pagination > .active > span, .pagination > .active > a:hover, .pagination > .active > span:hover, .pagination > .active > a:focus, .pagination > .active > span:focus
-  background:none
-  border-bottom: 1px solid
-  color: $accentColor
-.date
-  margin-left: 50px;
-  margin-top: -20px;
-.username
-  margin-top:-10px;
+.entry-image
+  grid-area img
+  object-fit cover
+  border-radius 1rem 0 0 1rem
+  margin -1.5rem
+  height: calc(3rem + 100%);
+
+.entry-link
+  color currentColor
+  text-decoration none
+  border-radius 1rem
+  display block
+
+.entry
+  margin 2rem
+  padding 1.5rem
+  box-shadow 0 2px 8px rgba(0,0,0,0.15)
+  border-radius 1rem
+  display grid
+  grid-template auto auto auto / 10rem auto
+  grid-template-areas "img title"\
+                      "img desc"\
+                      "img footer"
+
+.entry-header h2
+  margin 0
 </style>
